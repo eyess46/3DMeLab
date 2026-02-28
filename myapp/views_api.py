9bdc4a624118d5bf6253665a3842ef558e1dc80f
eyess46 <collinsonu3113@gmail.com>
2026-02-28 12:59:29 +0000

    changes made

--- myapp/views_api.py
+++ myapp/views_api.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import authenticate
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import User, VerificationCode
